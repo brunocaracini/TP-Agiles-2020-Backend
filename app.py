b76914291c07317ffb56982601679e09e127f0c1
Santiago Garcia <santiagogarcia97@outlook.com>
2020-10-26 18:16:24 -0300

    heroku config

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import flask
+from flask_cors import CORS, cross_origin
+
+port = int(os.environ.get("PORT", 5000))
+
+app = flask.Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello!'
+
+app.run(host='0.0.0.0', port=port, debug=True)
